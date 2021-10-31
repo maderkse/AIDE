e2c975c33a34bd038e87c63e7b773a5392e50c05
Ma Derkse <maderkse@gmail.com>
2021-10-31 17:28:32 +0100

    t6

--- Hello1/app/src/main/java/com/maderkse/apps/MainActivity.java
+++ Hello1/app/src/main/java/com/maderkse/apps/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends Activity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
-    {
+    {   //test6
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
